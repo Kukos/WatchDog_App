022b2646596d199798236ccf5c5b471c35a626ca
Michal Kukowski <michalkukowski10@gmail.com>
2017-07-08 16:27:40 +0200

    usage

--- src/main.c
+++ src/main.c
@@ -22,10 +22,35 @@ enum {
 #define OPT_NO_MATCH    0
 #define OPT_ERROR       -1
 
-/*
-    Wrapper for getopt_long_only, accept only exacly option, instead of part option
-*/
+/* Wrapper for getopt_long_only, accept only exacly option, instead of part option */
 int my_getopt_long_only(int argc, char *const *argv, const char *short_opt, const struct option *long_opt, int *index);
+
+/* Print help */
+void usage(void);
+
+void usage(void)
+{
+    (void)printf("HELP\n\n");
+    (void)printf("--dev [x]\t\t- change watchdog, default is /dev/watchdog\n");
+    (void)printf("--get-timeout\t\t- get timeout in seconds\n");
+    (void)printf("--set-timeout [x]\t- set timeout in seconds\n");
+    (void)printf("--get-pretimeout\t- get pretimeout in seconds\n");
+    (void)printf("--set-pretimeout [x]\t- set pretimeout in seconds\n");
+    (void)printf("--kepalive\t\t- feed watchdog\n");
+    (void)printf("--get-timeleft\t\t- get time left to reset in seconds\n");
+    (void)printf("--get-bootstatus\t- get bootstatus\n");
+    (void)printf("--get-status\t\t- get status\n");
+    (void)printf("--get-temp\t\t- get temperature in in degrees fahrenheit\n");
+    (void)printf("--set-options [x]\t- set options in hex\n");
+    (void)printf("--get-info\t\t- get info about WatchDog\n");
+    (void)printf("--help\t\t\t- print this usage\n");
+    (void)printf("\n");
+    (void)printf("Examples\n");
+    (void)printf("./watchdog --dev /dev/watchdog0 --set-timeout 8\n");
+    (void)printf("./watchdog --set-options 0x3\n");
+    (void)printf("./watchdog --get-info --get-temp --get-bootstatus --get-timeleft\n");
+    (void)printf("\n");
+}
 
 int my_getopt_long_only(int argc, char *const *argv, const char *short_opt, const struct option *long_opt, int *index)
 {
@@ -93,7 +118,10 @@ int main(int argc, char **argv)
 
     opterr = 0;
     if (argc < 2)
-        printf("Help\n");
+    {
+        usage();
+        return 1;
+    }
 
     while ((i = my_getopt_long_only(argc, argv, "", long_option, &index)) != -1)
     {
@@ -101,71 +129,71 @@ int main(int argc, char **argv)
         {
             case OPT_DEVICE:
             {
-                printf("Device\n");
+                (void)printf("Device\n");
                 break;
             }
             case OPT_GET_TIMEOUT:
             {
-                printf("GetTimeout\n");
+                (void)printf("GetTimeout\n");
                 break;
             }
             case OPT_SET_TIMEOUT:
             {
-                printf("SetTimeout\n");
+                (void)printf("SetTimeout\n");
                 break;
             }
             case OPT_GET_PRETIMEOUT:
             {
-                printf("GetPreTimeout\n");
+                (void)printf("GetPreTimeout\n");
                 break;
             }
             case OPT_SET_PRETIMEOUT:
             {
-                printf("SetPreTimeout\n");
+                (void)printf("SetPreTimeout\n");
                 break;
             }
             case OPT_KEEPALIVE:
             {
-                printf("KeepAlive\n");
+                (void)printf("KeepAlive\n");
                 break;
             }
             case OPT_GET_TIMELEFT:
             {
-                printf("GetTimeLeft\n");
+                (void)printf("GetTimeLeft\n");
                 break;
             }
             case OPT_GET_BOOTSTATUS:
             {
-                printf("GetBootStatus\n");
+                (void)printf("GetBootStatus\n");
                 break;
             }
             case OPT_GET_STATUS:
             {
-                printf("GetStatus\n");
+                (void)printf("GetStatus\n");
                 break;
             }
             case OPT_GET_TEMP:
             {
-                printf("GetTemp\n");
+                (void)printf("GetTemp\n");
                 break;
             }
             case OPT_SET_OPTIONS:
             {
-                printf("SetOptions\n");
+                (void)printf("SetOptions\n");
                 break;
             }
             case OPT_GET_INFO:
             {
-                printf("GetInfo\n");
+                (void)printf("GetInfo\n");
                 break;
             }
             case OPT_HELP:
             {
-                printf("Help\n");
+                (void)printf("Help\n");
                 break;
             }
             default:
-                printf("Help\n");
+                usage();
         }
     }
 
